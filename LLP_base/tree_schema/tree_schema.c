ef8a0d7348bbbb05f467db30e596ffbd89c3ed15
panel14 <xdotawinx@gmail.com>
2022-11-01 20:46:34 +0300

    Changed tree_schema

--- LLP_base/tree_schema/tree_schema.c
+++ LLP_base/tree_schema/tree_schema.c
@@ -3,19 +3,7 @@
 struct tree_schema
 {
 	int tree_size;
+	int nodes_count;
+
 	struct key_node* root;
 };
-
-int create_tree() {
-
-	return 0;
-}
-
-int delete_tree() {
-
-}
-
-int add_node(struct key_node node) {
-
-	return 0;
-}
